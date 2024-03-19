b2efe453e5eb2f8e069b8a9b7fbfe16624144c71
Ali Helmi <ahelmi365@gmail.com>
2024-03-19 14:56:07 +0200

    handle function to start and stop worker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
+localStorage.setItem("apiWorker", false);
+const startWorkerBtn = document.getElementById("startWorker");
+const stopWorkerBtn = document.getElementById("stopWorker");
 let apiWorker;
 
 // Cleanup function
 const stopWorker = () => {
   // Terminate the worker when the component unmounts
-  apiWorker.terminate();
+  apiWorker?.terminate();
+  localStorage.setItem("apiWorker", false);
+
+  enableBtnElement(startWorkerBtn, "start-btn");
+  disableBtnElement(stopWorkerBtn, "stop-btn");
 };
 
 const startWorkder = () => {
   // Create the worker
   apiWorker = new Worker("../public/worker.js");
+  if (apiWorker) {
+    localStorage.setItem("apiWorker", true);
+
+    disableBtnElement(startWorkerBtn, "start-btn");
+    enableBtnElement(stopWorkerBtn, "stop-btn");
+  }
   console.log({ apiWorker });
   // Send message to the worker with the interval
   const interval = 5000; // 5 seconds
@@ -38,13 +51,25 @@ const addTodosList = (todos) => {
 };
 
 // start worker
-const startWorkerBtn = document.getElementById("startWorker");
+
 startWorkerBtn.addEventListener("click", () => {
   startWorkder();
 });
 
 // end worker
-const endWorkerBtn = document.getElementById("endWorker");
-endWorkerBtn.addEventListener("click", () => {
+stopWorkerBtn.addEventListener("click", () => {
   stopWorker();
 });
+
+// disable "stop worker" btn if not worker
+const isApiWorker = localStorage.getItem("apiWorker");
+console.log({ isApiWorker });
+
+const disableBtnElement = (btnElement, btnClassNameToRemove) => {
+  btnElement.setAttribute("disabled", true);
+  btnElement.classList.remove(btnClassNameToRemove);
+};
+const enableBtnElement = (btnElement, btnClassNameToRemove) => {
+  btnElement.removeAttribute("disabled");
+  btnElement.classList.add(btnClassNameToRemove);
+};
